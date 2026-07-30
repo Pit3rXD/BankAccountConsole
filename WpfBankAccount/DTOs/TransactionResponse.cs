e414f724a9018db757e9bcd2f30536056dfca950
Piotrek <piepietras@gmail.com>
2026-07-30 22:14:03 +0200

    feat: show transfer counterparty in transaction history

--- WpfBankAccount/DTOs/TransactionResponse.cs
+++ WpfBankAccount/DTOs/TransactionResponse.cs
@@ -5,6 +5,7 @@ namespace WpfBankAccount.DTOs
     public class TransactionResponse
     {
         public int Id { get; set; }
+        public string CounterpartyAccountNumber { get; set; } = string.Empty;
         public decimal Amount { get; set; }
         public DateTime Date { get; set; }
         public TransactionType Type { get; set; }
